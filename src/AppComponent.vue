7ff6ab5d6015bc1ef92861e2871e60721ed0daad
ejguan123 <ej_guan@tk3c.com>
2024-12-24 15:49:09 +0800

    新格式

--- src/AppComponent.vue
+++ src/AppComponent.vue
@@ -1,26 +1,43 @@
 <script setup>
+// 各頁面切換
+
+/* 綠點 */
 //import green2 from '@/views/view/common/green2.vue'
-//import discount from '@/views/view/common/discount.vue'
+
+/* 空調 */
 //import air2410 from '@/views/view/air/air2410.vue'
-//import night from '@/views/view/common/night.vue'
+
+/* 電視 */
 //import tv2412 from '@/views/view/tv/tv2412.vue'
+
+/* 夜殺 */
+//import night from '@/views/view/common/night.vue'
+
+/* 冰箱洗衣機 */
 //import ice2412 from '@/views/view/ice/ice2412.vue'
-//import ai2412 from '@/views/view/ai/Ai2412.vue'
-//import Thank from '@/views/view/Thank.vue'
-//import Banquet from '@/views/view/Banquet.vue'
-//import DjiAir from '@/views/view/DjiAir.vue'
-//import Black from '@/views/view/Black.vue'
+
+/* 風扇 */
 //import Fan from '@/views/view/fan/Fan.vue'
+
+/* 銀行 */
 //import bank from '@/views/view/common/bank.vue'
-//import Warm from '@/views/view/common/Warm.vue'
-import double12 from '@/views/view/double12.vue'
-//import CNair from '@/views/view/staff/CNair.vue'
-//import EvaAir from '@/views/view/staff/EvaAir.vue'
+
+/* 匯聚 */
 //import clean2412 from '@/views/view/clean/clean2412.vue'
-//import Tup from '@/views/view/staff/Tup.vue'
+
+/* ai 系統 */
+//import Ai2412 from '@/views/view/ai/Ai2412.vue'
+
+/* 聖誕 */
+//import xmas from '@/views/view/xmas.vue'
+
+/* 電暖器 */
+//import Warm from '@/views/view/common/Warm.vue'
+
+import Banquet from '@/views/view/Banquet.vue'
 </script>
 
 <template>
   <!-- 主內容區 -->
-  <component :is="double12"></component>
+   <component :is="Banquet"></component>
 </template>
