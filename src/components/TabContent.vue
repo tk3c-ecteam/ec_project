6714a2fdc497c387ea9922ade6d97c946da8f2b7
ejguan123 <ej_guan@tk3c.com>
2024-12-31 17:55:34 +0800

    新增頁籤共用功能

---
+++ src/components/TabContent.vue
@@ -0,0 +1,64 @@
+<template>
+  <transition appear>
+    <div class="tab-content" v-if="isActive">
+      <slot>
+        <a v-if="banners != undefined" class="banner" :href="$filters.addGALink(banners.url)" target="_blank">
+          <img class="pc" :src="$filters.siteUrl(banners.pc)" alt=" ">
+          <img class="mobile" :src="$filters.siteUrl(banners.mobile)" alt=" ">
+        </a>
+        <listF v-if="isSwiper && products[menus[index]] != undefined" :isSwiper="1" :pro="products[menus[index]].Data" :name="`box${index + 1}`"></listF>
+         <listF v-else-if="products[menus[index]] != undefined" :pro="products[menus[index]].Data"></listF>
+      </slot>
+    </div>
+  </transition>
+</template>
+
+<script>
+import { globalMixin } from '../globalMixin.js';
+import listF from '@/views/layout/listF.vue';
+export default {
+  mixins: [globalMixin],
+  props: {
+    index: {
+      type: Number,
+      required: true
+    },
+    selectedTab: {
+      type: Number,
+      required: true
+    },
+    menus: {
+      type: Object,
+      required: true
+    },
+    banners:{
+      type:Object
+    },
+    isSwiper:{
+      type:Boolean,
+      default:0
+    }
+  },
+  components: {
+    listF
+  },
+  mounted() {
+    this.getFloorData(this.menus);
+  },
+  computed: {
+    isActive() {
+      return this.index === this.selectedTab;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+  .v-enter-active, .v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from, .v-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ src/components/Tabs.vue
@@ -0,0 +1,121 @@
+<template>
+   <!--
+    * 頁籤無輪播滑動
+    * props:
+       tabs -> 頁籤按鈕(文字或圖片)與連結 的object 格式
+       isSwiper -> 是否為輪播
+       textOrImage = 'text' 頁籤按鈕為文字
+      預設為圖片
+      singleUrl -> 連結只有一個(如果有頁籤也沒有連結)
+      statusSelect-> 預設頁籤要顯示幾個(都沒有設定為0)
+    */ -->
+
+    <div class="tab mb:2%">
+      <ul v-if="isSwiper == false" class="gap:10">
+        <li v-for="(tab, index) in tabs" :key="index" :class="{ active: selectedTab === index }"
+          class="brightness(0.7) brightness(1).active">
+            <a v-if="singleUrl != undefined" :value="$filters.addGALink(singleUrl)" @click.prevent="selectTab(index)">
+            <b v-if="textOrImage == 'text'"> {{ text }}</b>
+            <img v-else :src="$filters.siteUrl(tab.image)" alt=" " />
+          </a>
+          <a v-else :value="$filters.addGALink(tab.url)" @click.prevent="selectTab(index)">
+             <b v-if="textOrImage == 'text'"> {{ text }}</b>
+            <img v-else :src="$filters.siteUrl(tab.image)" alt=" " />
+          </a>
+        </li>
+      </ul>
+
+      <!-- swiper有輪播滑動版本 -->
+      <swiper v-else 
+      :loop="false" 
+      :space-between="10" 
+      :slides-per-view="'auto'"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+      >
+        <swiper-slide v-for="(tab, index) in tabs" :key="index" :class="{ active: selectedTab === index }" class="flex! flex-basis:fit brightness(0.7) brightness(1).active" @click="goSlide(index)">
+           <a v-if="singleUrl != undefined" :value="$filters.addGALink(singleUrl)"  @click.prevent="selectTab(index)" >
+             <b v-if="textOrImage == 'text'"> {{ text }}</b>
+            <img v-else :src="$filters.siteUrl(tab.image)" alt=" " />
+          </a>
+            <a v-else :value="$filters.addGALink(tab.url)"  @click.prevent="selectTab(index)">
+              <b v-if="textOrImage == 'text'"> {{ text }}</b>
+            <img v-esle :src="$filters.siteUrl(tab.image)" alt=" " />
+          </a>
+        </swiper-slide>
+      </swiper>
+    </div>
+
+    <slot :selectedTab="selectedTab"></slot>
+</template>
+
+<script>
+export default {
+  props: {
+    tabs: {
+      type: Object,
+      required: true
+    },
+    isSwiper: {
+      type: Boolean,
+      default: false
+    },
+    textOrImage: {
+      type: String,
+      default: 'image'
+    },
+    singleUrl:{
+      type:String
+    },
+    statusSelect:{
+      type:Number
+    }
+  },
+  data() {
+    return {
+      selectedTab: 0,
+      swiper:null
+    };
+  },
+  mounted() {
+    if (this.statusSelect != undefined) this.selectedTab = this.statusSelect
+  },
+  methods: {
+    selectTab(index) {
+      this.selectedTab = index;
+
+       if (event) {
+        let current = event.currentTarget,
+        parent = '';
+
+  
+         if (current.getAttribute('value') == '') return false;
+         
+        //頁籤為swiper(輪播)格式
+        if (current.parentNode.getAttribute('class') == 'swiper-slide') {
+          parent = current.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+        } else {
+          parent = current.parentNode.parentNode.parentNode.parentNode.parentNode;
+        }
+
+      
+        let parentAr = parent,
+          parentClass = parentAr.getAttribute('class'),
+          parentName = parentClass.substr(16),
+          getUrl = current.getAttribute('value');
+  
+        document.querySelector(`.${parentName} .title a`).setAttribute('href', getUrl);
+      }
+    },
+    onSwiper(swiper) {
+      this.swiper = swiper;
+    },
+    onSlideChange() {
+      this.selectedTab = this.swiper.activeIndex;
+    },
+    goSlide(index) {
+      this.swiper.slideTo(index);
+    }
+  }
+};
+</script>
