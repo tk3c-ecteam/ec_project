7ad430f5367fc93241fe8e1f6cf7571a72035476
ejguan123 <ej_guan@tk3c.com>
2025-01-09 13:22:26 +0800

    新增 auto-import 自動引入套件

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import { resolve } from 'node:path'
 import autoprefixer from 'autoprefixer'
 import { createHtmlPlugin } from 'vite-plugin-html'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +15,29 @@ export default defineConfig({
       script: {
         defineModel: true
       }
+    }),
+    //自動引入import 套件
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      imports: [
+        'vue',
+        'vue-router',
+        {
+          'axios': ['axios'], //import axios from 'axios'
+        }
+      ]
+    }),
+    Components({
+      //要引入的component路徑
+      dirs: ['src/components', 'src/views/floor', 'src/views/layout'],
+      extensions: ['vue'],
+      deep: true,//搜尋子目錄
+      dts: 'src/components.d.ts'
     }),
     createHtmlPlugin({
       minify: true,
