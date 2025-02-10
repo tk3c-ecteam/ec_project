a4a6aeca06d2e71e96d2ef3d993a7bb530058c74
ejguan123 <ej_guan@tk3c.com>
2025-02-10 17:16:19 +0800

    更新註解

--- src/views/floor/CommonFloor.vue
+++ src/views/floor/CommonFloor.vue
@@ -4,9 +4,10 @@ import listF from '@/views/layout/listF.vue'
 
 <script>
 /* 商品樓層共用版型
- * 傳入物件: floors(樓層標題圖片、連結) , menus(樓層陳列編號),
- * singleImage(純標題圖片單張+系統文字)
- * isSwiper(是否使用swiper輪播)
+ * 傳入物件: floors(樓層標題圖片、連結) , menus(樓層陳列編號) - 必要
+ * singleImage(純標題圖片單張+系統文字) - 非必要
+ * isSwiper(是否使用swiper輪播) - 非必要 (有要輪播才要寫 isSwiper = 1)
+ * moreImage(看更多按鈕圖片) - 非必要
  */
 export default {
   props: ['floors', 'menu', 'singleImage','moreImage','isSwiper'],
@@ -17,7 +18,7 @@ export default {
 </script>
 
 <template>
-  <section class="floor scroll" v-for="(floor, f) in floors" :key="f">
+  <section class="floor scroll" v-for="(floor, f) in floors" :titles="[products[menu[f]] != undefined ? products[menu[f]].MenuTitle.trim() : '']" :key="f">
     <h2 class="title" :name="`pro${menu[f]}`" :id="`pro${menu[f]}`">
       <!-- 圖片+連結標題 -->
       <a v-if="floor.url" :href="$filters.addGALink(floor.url)">
@@ -35,7 +36,7 @@ export default {
     <!-- 有輪播 -->
     <div class="content" v-if="isSwiper">
       <component v-if="products[menu[f]] != undefined && isSwiper" :is="floor.type != undefined ? floor.type : listF"
-        :pro="products[menu[f]].Data" :isSwiper="isSwiper" :name="`pro${f + 1}`"></component>
+        :pro="products[menu[f]].Data" :isSwiper="1" :name="`pro${f + 1}`"></component>
     </div>
 
     <div class="content" v-else>
