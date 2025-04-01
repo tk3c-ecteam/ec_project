0344d9bb4c4ff9cb817e721164071c59b3cc2bf4
ejguan123 <ej_guan@tk3c.com>
2025-04-01 15:19:31 +0800

    新增上方上品輪播版型

---
+++ src/views/layout/ProductTop.vue
@@ -0,0 +1,97 @@
+<script>
+export default {
+  props: ['pro']
+}
+</script>
+
+<!-- 上方商品輪播 -->
+<template>
+  <!-- 有輪播 -->
+  <div class="bg01 list_F bg:#fff r:15px r:10px@<576 p:1%|1%|0! p:2%|2%|0!@<576 box:border-box">
+    <ul v-if="pro != undefined" class="pro-top">
+      <swiper
+        :loop="false"
+        :autoplay="{
+          delay: 2200,
+          disableOnInteraction: false
+        }"
+        :space-between="10"
+        :slidesPerView="'auto'"
+        :pagination="{
+          el:'.pro-top .page'
+        }"
+      >
+        <swiper-slide
+          v-for="(proA, p) in pro"
+          class="bg:#fff pb:2%@<576"
+        >
+          <a
+            :href="$filters.addGALink('https://www.tk3c.com/pt.aspx?pid=' + proA.productid)"
+            :id="'prod' + proA.productid"
+            :name="'prod' + proA.productid"
+          >
+            <p class="itemF_img"><img onerror="ImgError(this);" :src="proA.ImgUrl" border="0" /></p>
+            <storg v-html="proA.productname"></storg>
+            <h4 :class="[hidePromte(proA.Promote) == '' ? 'empty' : '']">{{ hidePromte(proA.Promote) }}</h4>
+            <div class="boxF_price">
+              <p class="iconF_pro" v-if="getProPercent(proA) != 100">
+                <span>{{ getProPercent(proA) }}</span
+                >折
+              </p>
+
+              <strong class="txt_red fred">
+                <em v-if="proA.nonmemberprice != proA.realprice">市價${{ addNumComma(proA.nonmemberprice) }}</em>
+                <i>活動價$</i>{{ addNumComma(proA.realprice) }}
+              </strong>
+            </div>
+          </a>
+        </swiper-slide>
+      </swiper>
+      <div v-if="pro.length > 1" class="swiper-pagination page"></div>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss">
+  .pro-top {
+        .boxF_price {
+          strong {
+            em,
+            &:before {
+              display: none;
+            }
+          }
+        }
+        .iconF_pro {
+          display: none;
+        }
+        .boxF_price {
+          strong {
+            height: 60px;
+            line-height: 40px;
+          }
+        }
+        .swiper-wrapper {
+            justify-content: center;
+            align-items:baseline;
+          }
+  }
+
+  @include media-query('mobile', '576px') {
+    .pro-top {
+      border-radius: 10px;
+      box-sizing: border-box;
+          .swiper-wrapper {
+            justify-content: left;
+          }
+          .boxF_price {
+            &:after {
+              display: none;
+            }
+            strong {
+              height: 35px;
+            }
+          }
+    }
+  }
+</style>
