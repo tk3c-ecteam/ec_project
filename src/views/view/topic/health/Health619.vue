08499c16c61c6d5cafa941adc2eaeba28a9f1397
ejguan123 <ej_guan@tk3c.com>
2025-06-13 16:36:09 +0800

    update

---
+++ src/views/view/topic/health/Health619.vue
@@ -0,0 +1,71 @@
+<template>
+  <Template618 :contents="contents">
+    <template #items>
+      <p class="items i1">
+        <img :src="$filters.siteUrl('healthbeauty/images/item1.png')" />
+      </p>
+      <p class="items i2">
+        <img :src="$filters.siteUrl('healthbeauty/images/item4.png')" />
+      </p>
+      <p class="items i3">
+        <img :src="$filters.siteUrl('healthbeauty/images/item2.png')" />
+      </p>
+      <p class="items i4">
+        <img :src="$filters.siteUrl('healthbeauty/images/item3.png')" />
+      </p>
+    </template>
+
+    <template #summer-floor>
+      <img :src="$filters.siteUrl('fan/images/S03_a.png')" />
+    </template>
+  </Template618>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      today: new Date(),
+      contents: [
+        {
+          type:'summer-other',
+          menuPro: 8261,
+          title: 'healthbeauty/images/title619.png',
+          floors: [
+            {
+              text: '吹風機',
+              url:'https://www.tk3c.com/dic1.aspx?cid=4640&aid=2488'
+            },
+            {
+              text: '按摩椅',
+              url:'https://www.tk3c.com/dic1.aspx?cid=842&aid=2492'
+            },
+            {
+              text: '刮鬍刀',
+              url: "https://www.tk3c.com/dic1.aspx?cid=4640&aid=2489"
+            },
+            {
+              text: '筋膜槍',
+              url: "https://www.tk3c.com/dic1.aspx?cid=842&aid=2492"
+            },
+            {
+              text: '體脂計',
+              url: "https://www.tk3c.com/dic1.aspx?cid=108783&aid=23072"
+            },
+            {
+              text: '口腔清潔',
+              url: "https://www.tk3c.com/dic1.aspx?cid=4640&aid=2491"
+            },
+            {
+              text: '便座',
+              url: "https://www.tk3c.com/dic1.aspx?cid=108783&aid=23230"
+            },
+          ],
+          menus: [8250,8251,8252,8253,8254,8255,8260]
+        }
+      ],
+    }
+  }
+}
+</script>
+
---
+++ src/views/view/topic/office/Office619.vue
@@ -0,0 +1,94 @@
+<template>
+  <Template618 :contents="contents">
+    <template #items>
+        <p class="items i1">
+          <img :src="$filters.siteUrl('office3c/images/item1_a.png')" />
+        </p>
+        <p class="items i2">
+          <img :src="$filters.siteUrl('office3c/images/item2_a.png')" />
+        </p>
+      </template>
+
+      <template #summer-floor>
+        <img :src="$filters.siteUrl('fan/images/S01_a.png')" />
+      </template>
+  </Template618>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      today: new Date(),
+      contents: [
+        {
+          type:'summer-other',
+          menuPro: 8256,
+          title: 'office3c/images/title619_a.png',
+          floors: [
+            {
+              text: '噴墨印表機',
+              url: "https://www.tk3c.com/dictitleurl.aspx?cid=16917"
+            },
+            {
+              text: '雷射印表機',
+              url: "https://www.tk3c.com/dictitleurl.aspx?cid=83623"
+            },
+            {
+              text: '印表機耗材',
+              url: "https://www.tk3c.com/dic1.aspx?cid=16917&aid=19080"
+            },
+            {
+              text: '鍵盤',
+              url: "https://www.tk3c.com/dic1.aspx?cid=14719&aid=10020"
+            },
+            {
+              text: '滑鼠',
+              url: "https://www.tk3c.com/dic1.aspx?cid=14719&aid=6040"
+            },
+            {
+              text: '螢幕',
+              url: "https://www.tk3c.com/dictitleurl.aspx?cid=44823"
+            },
+            {
+              text: '路由器',
+              url: "https://www.tk3c.com/dictitleurl.aspx?cid=43403"
+            },
+            {
+              text: '升降桌',
+              url: "https://www.tk3c.com/dic1.aspx?cid=11684&aid=23765"
+            },
+            {
+              text: '護貝/碎紙/點鈔機',
+              url: "https://www.tk3c.com/dictitleurl.aspx?cid=11684"
+            },
+            {
+              text: '延長線/電池',
+              url: "https://www.tk3c.com/dic1.aspx?cid=43450&aid=11643"
+            },
+          ],
+          menus: [8245,8246,8247,8248,8249,8258,8257,8259,8262,8263]
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  body {
+    background: #cfffcd;
+  }
+
+  .bg01 {
+    background: #1084ec;
+  }
+
+  .floor {
+    .title {
+      b {
+        top: 43%;
+      }
+    }
+  }
+</style>
